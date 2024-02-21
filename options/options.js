d1873ed931a30fa6dc893b7d4df0c1917f15e731
Crowdbotics <team@crowdbotics.com>
2024-02-21 22:18:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fitjourney-46553",
+  url: "https://fitjourney-46553.botics.co",
+  api: "https://fitjourney-46553.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
